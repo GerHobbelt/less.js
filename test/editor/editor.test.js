7974fb0a053c3f3cf198fdd462f6bf7248eba85f
Francisco López <donflopez@gmail.com>
2017-05-19 12:20:22 +0200

    Remove editor.test.js

--- test/editor/editor.test.js
+++
@@ -1,8 +0,0 @@
-var ref = require('./reference.json');
-var carto = require('../../lib/carto/index.js');
-
-
-var ccss = new carto.RendererJS({
-    reference: ref,
-    strict: true
-}).render("#layer { polygon-fill: #abcdef; }");
